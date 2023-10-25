8e8cde013f5809d4242f38e14cf369c8469cb8f6
Torremolinos <adrian_2054@hotmail.com>
2023-10-25 08:47:36 +0200

    añado variables y funciones

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,30 @@
  */
 /*  <!--funcionalidades del cajero, *depositar(), *retirar(), *transferir(), *iniciar sesion(), *operaciones del Cajero()-->*/
 
+/*Lo primero seria tener en mente el SALDO. Que será una variable que cambie.*/
+/*Luego otra que seria el pin, se pondra en mayusculas PIN_CORRECTO*/
+/*luego estarian los intentos. Que seran 3.*/
+
+let saldo = 1000; //estarian en una base de datos
+const PIN_CORRECTO = "1234"; //lo mismo que la de arriba.
+let intentosRestantes = 3;
+
+function mostrarSaldo() {
+
+}
+function depositarSaldo() {
+
+}
+function retirar() {
+
+}
+function transferir() {
+
+}
+function iniciarSesion() { //esto seria nuestro inicio por lo evidente.
+
+}
+function operacionesCajero() { //esto seria el menu.
+
+} 
+
